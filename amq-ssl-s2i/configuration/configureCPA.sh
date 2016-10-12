2e9ab779b645f09dce2ca884ce301f594fd248f1
samuel-dev <samuel_c_lee@cathaypacific.com>
2016-10-12 15:43:23 +0800

    Create configureCPA.sh

---
+++ amq-ssl-s2i/configuration/configureCPA.sh
@@ -0,0 +1,1 @@
+sed -i "s|<transportConnector name=\"ssl\" uri=\"ssl://0.0.0.0:61617?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\" \/>|<transportConnector name=\"ssl\" uri=\"ssl://0.0.0.0:61617?needClientAuth=true\&amp;maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\" \/>|" $AMQ_HOME/conf/activemq.xml
